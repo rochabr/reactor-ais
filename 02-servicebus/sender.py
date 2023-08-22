fb85d37076718aa16ae65f691c8fae8591c862de
Fernando Rocha <fernandorsl@gmail.com>
2023-08-21 20:49:20 -0700

    Update sender.py

--- 02-servicebus/sender.py
+++ 02-servicebus/sender.py
@@ -1,19 +1,34 @@
+import os
 from azure.servicebus import ServiceBusClient, ServiceBusMessage
 
-import os
+CONNECTION_STR = os.environ["SERVICEBUS_CONN_STR"]
+QUEUE_NAME = os.environ["QUEUE_NAME"]
 
-connstr = os.environ["SERVICEBUS_CONN_STR"]
-queue_name = os.environ["QUEUE_NAME"]
+def send_single_message(sender):
+    message = ServiceBusMessage("Single Message")
+    sender.send_messages(message)
 
-with ServiceBusClient.from_connection_string(connstr) as client:
-    with client.get_queue_sender(queue_name) as sender:
-        # Sending a single message
-        single_message = ServiceBusMessage("Single message")
-        sender.send_messages(single_message)
+def send_a_list_of_messages(sender):
+    messages = [ServiceBusMessage("Message in list") for _ in range(10)]
+    sender.send_messages(messages)
 
-        # Sending a list of messages
-        messages = [
-            ServiceBusMessage("First message"),
-            ServiceBusMessage("Second message"),
-        ]
-        sender.send_messages(messages)
+def send_batch_message(sender):
+    batch_message = sender.create_message_batch()
+    for _ in range(10):
+        try:
+            batch_message.add_message(ServiceBusMessage("Message inside a ServiceBusMessageBatch"))
+        except ValueError:
+            # ServiceBusMessageBatch object reaches max_size.
+            # New ServiceBusMessageBatch object can be created here to send more data.
+            break
+    sender.send_messages(batch_message)
+
+servicebus_client = ServiceBusClient.from_connection_string(conn_str=CONNECTION_STR, logging_enable=True)
+with servicebus_client:
+    sender = servicebus_client.get_queue_sender(queue_name=QUEUE_NAME)
+    with sender:
+        send_single_message(sender)
+        send_a_list_of_messages(sender)
+        send_batch_message(sender)
+
+print("Send message is done.")
