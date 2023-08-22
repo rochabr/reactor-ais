9245b183ec8af1478c68f6e55aabb3bd70553868
Fernando Rocha <fernandorsl@gmail.com>
2023-08-21 20:46:28 -0700

    Update receiver.py

--- 02-servicebus/receiver.py
+++ 02-servicebus/receiver.py
@@ -2,13 +2,13 @@ from azure.servicebus import ServiceBusClient
 
 import os
 
-connstr = os.environ["SERVICEBUS_CONN_STR"]
-queue_name = os.environ["QUEUE_NAME"]
+CONNECTION_STR = os.environ["SERVICEBUS_CONN_STR"]
+QUEUE_NAME = os.environ["QUEUE_NAME"]
 
-with ServiceBusClient.from_connection_string(connstr) as client:
+with ServiceBusClient.from_connection_string(CONNECTION_STR) as client:
     # max_wait_time specifies how long the receiver should wait with no incoming messages before stopping receipt.
     # Default is None; to receive forever.
-    with client.get_queue_receiver(queue_name, max_wait_time=30) as receiver:
+    with client.get_queue_receiver(QUEUE_NAME, max_wait_time=30) as receiver:
         for msg in receiver:  # ServiceBusReceiver instance is a generator.
             print(str(msg))
             # receiver.complete_message(msg)
